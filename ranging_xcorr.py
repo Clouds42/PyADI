5dee530203c506ee8a546946e73562b117ab61f6
Clouds42 <clouds42@foxmail.com>
2024-07-03 10:55:36 +0800

    add env control

--- ranging_xcorr.py
+++ ranging_xcorr.py
@@ -6,21 +6,30 @@ import matplotlib.pyplot as plt
 import queue
 from sdr_func import *
 
+local = 1  # 1 or 0
+single = 0  # 1 or 0
+plot = 0
 
 sdr = adi.Pluto("ip:ant.local")
 sdr.sample_rate = 1000000
-sdr.gain_control_mode_chan0 = 'manual'
-sdr.rx_hardwaregain_chan0 = 64
-sdr.rx_buffer_size = 100000
-sdr.rx_lo = 915000000
-sdr.rx_rf_bandwidth = 20000000
-sdr.tx_rf_bandwidth = 20000000
-sdr.tx_lo = 2400000000
-sdr.tx_hardwaregain_chan0 = 0
-sdr.tx_cyclic_buffer = True
 
-data = np.array([1, 1, 1, -1, -1, 1, -1])
-tx_samples = data*(2**14)
+sdr.gain_control_mode_chan0 = 'manual'
+sdr.rx_lo = 2400000000 if local == 1 else 915000000
+sdr.rx_rf_bandwidth = 20000000
+sdr.rx_hardwaregain_chan0 = 64
+sdr.rx_buffer_size = 4000
+
+sdr.tx_lo = 2400000000
+sdr.tx_rf_bandwidth = 20000000
+sdr.tx_hardwaregain_chan0 = 0
+# sdr.tx_cyclic_buffer = True
+
+epoch = 1 if single == 1 else 100
+start_index = 0 if local == 1 else 40000
+
+frame = np.array([1, 1, 1, -1, -1, 1, -1])
+data = np.tile(frame, 10)
+tx_samples = data * (2**14)
 
 
 def tx():
@@ -33,9 +42,9 @@ def rx(q):
 
 
 def main():
-    verbose = 1 # 1 or 0
+    start_time = time.time()
+
     i = 0
-    epoch = 1 if verbose == 1 else 10
     result = np.zeros(epoch)
     results_queue = queue.Queue()
 
@@ -54,20 +63,24 @@ def main():
         threhold = np.mean([0, np.max(rx_samples_real)])
 
         rx_samples_dec = np.where(rx_samples_real > threhold, 1,
-                        np.where(rx_samples_real < np.negative(threhold), -1, 0))
+                                  np.where(rx_samples_real < np.negative(threhold), -1, 0))
 
-        array = np.array([1, 1, 1, -1, -1, 1, -1])
-        correlation = np.correlate(rx_samples_dec, array, mode='full')
-        index = find_frame(correlation, 0)
-        print(index)
+        correlation = np.correlate(rx_samples_dec, frame, mode='full')
+        index = find_frame(correlation, 0, 0)
+        print(f"The {i}-th frame detected. Index: {index}")
         result[i] = index
 
         i = i + 1
 
     print(result)
-    mean = np.mean(result)
-    print(mean)
-    if verbose == 1:
+    if single == 0:
+        mean = np.mean(result[1:])
+        print(mean)
+
+    end_time = time.time()
+    print(end_time - start_time)
+
+    if plot == 1:
         plt.figure("Result")
         plt.grid()
 
@@ -88,7 +101,7 @@ def main():
     else:
         plt.plot(result, 'o-')
         plt.grid()
-        plt.ylim([0, 70000])
+        plt.ylim([0, 4000])
         plt.show()
 
 
